c3cd90e131896d67a68f67ed3a688376babc8d57
Jose <santosjosel1995@gmail.com>
2020-11-11 14:45:08 -0500

    Header format and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="header">
+      <h1 className="header-title">React Super Conference</h1>
+      <ul className="header-nav">
+        <li className="header-nav--item">Home</li>
+        <li className="header-nav--item">Location</li>
+        <li className="header-nav--item">Food</li>
+      </ul>
     </div>
   )
 }
