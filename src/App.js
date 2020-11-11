9a04fe8dd0a89ca892526a21b3dae27bef00166d
Jose <santosjosel1995@gmail.com>
2020-11-11 12:18:14 -0500

    Set up base components and style page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './styles.css';
+import Header from './components/Header';
+import Event from './components/Event';
+import Speaker from './components/Speaker';
 
 const App = () => {
   return (
     <div>
-      App
+      <Header />
+      <Event />
+      <Speaker />
     </div>
   )
 }
