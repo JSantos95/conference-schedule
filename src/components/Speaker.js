3e3a4e41297b47d872b4c20766824487ca719e98
Jose <santosjosel1995@gmail.com>
2020-11-13 13:21:46 -0500

    Speaker format and styling

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Faker from 'faker';
+import Person from './Person';
 
 const Speaker = () => {
+
+  const [nameList, setList] = useState([]);
+
+  // eslint-disable-next-line
+  useEffect(() => {
+    let arr = [];
+    for(let i = 0; i < 15; i++) {
+      let user = {
+        id: i,
+        name: Faker.name.firstName() + ' ' + Faker.name.lastName(),
+        picture: Faker.image.avatar()
+      }
+      arr.push(user);
+    }
+    setList(arr);
+  }, []);
+
+  const speakers = nameList.map(obj =>
+    <Person key={obj.id} name={obj.name} pic={obj.picture} />
+  )
+
   return (
-    <div>
-      Speaker
+    <div className="speaker">
+      {speakers}
     </div>
   )
 }
