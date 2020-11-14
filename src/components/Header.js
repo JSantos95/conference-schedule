7f1e459b5c6dcec5b65da0bc8daba6d96fb8a3c1
Jose <santosjosel1995@gmail.com>
2020-11-13 23:48:54 -0500

    Changes to font and sizes of the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="header-title">React Super Conference</h1>
+      <h1 className="header-title">Techathon</h1>
       <ul className="header-nav">
         <li className="header-nav--item">Home</li>
         <li className="header-nav--item">Location</li>
