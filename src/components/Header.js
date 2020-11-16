d39ec3a65c34f962b3c4b27467bd875d068ce2e9
Jose <santosjosel1995@gmail.com>
2020-11-15 21:11:56 -0500

    Changes to the Header style and layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="header">
-      <h1 className="header-title">Techathon</h1>
-      <ul className="header-nav">
-        <li className="header-nav--item">Home</li>
-        <li className="header-nav--item">Location</li>
-        <li className="header-nav--item">Food</li>
-      </ul>
+    <div className="header-con">
+      <div className="header">
+        <h1 className="header-title">Techathon</h1>
+        <ul className="header-nav">
+          <li className="header-nav--item">Home</li>
+          <li className="header-nav--item">Location</li>
+          <li className="header-nav--item">Food</li>
+        </ul>
+      </div>
     </div>
   )
 }
