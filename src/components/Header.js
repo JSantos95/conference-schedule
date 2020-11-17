770a5ad29cf1e04aa79e69b320ad5248e383ae1b
Jose <santosjosel1995@gmail.com>
2020-11-17 13:54:54 -0500

    Header now useing react-router-dom Link class

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="header-con">
       <div className="header">
+
+      <Link to="/" className="header-title-link">
         <h1 className="header-title">Techathon</h1>
+      </Link>
+
+
         <ul className="header-nav">
-          <li className="header-nav--item">Home <i class="fas fa-home"></i></li>
-          <li className="header-nav--item">Location <i class="fas fa-map-marker-alt"></i></li>
-          <li className="header-nav--item">Food <i class="fas fa-utensils"></i></li>
+          <li className="header-nav--item">
+            <Link to="/" className="header-nav--link">
+              Home <i class="fas fa-home"></i>
+            </Link>
+          </li>
+          <li className="header-nav--item">
+            <Link to="/streams/info" className="header-nav--link">
+              Location <i class="fas fa-map-marker-alt"></i>
+            </Link>
+          </li>
+          <li className="header-nav--item">
+            <Link to="streams/schedule" className="header-nav--link">
+            Schedule <i class="fas fa-calendar-week"></i>
+            </Link>
+          </li>
         </ul>
+
       </div>
     </div>
   )
