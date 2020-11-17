47000d82bbc4fdb169443d575085829e6849373d
Jose <santosjosel1995@gmail.com>
2020-11-17 16:46:22 -0500

    Swapped the Info and Schedule tab, and corrected the Info tab ℹ

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,9 @@ const Header = () => {
     <div className="header-con">
       <div className="header">
 
-      <Link to="/" className="header-title-link">
-        <h1 className="header-title">Techathon</h1>
-      </Link>
-
+        <Link to="/" className="header-title-link">
+          <h1 className="header-title">Techathon</h1>
+        </Link>
 
         <ul className="header-nav">
           <li className="header-nav--item">
@@ -18,13 +17,13 @@ const Header = () => {
             </Link>
           </li>
           <li className="header-nav--item">
-            <Link to="/streams/info" className="header-nav--link">
-              Location <i class="fas fa-map-marker-alt"></i>
+            <Link to="streams/schedule" className="header-nav--link">
+            Schedule <i class="fas fa-calendar-week"></i>
             </Link>
           </li>
           <li className="header-nav--item">
-            <Link to="streams/schedule" className="header-nav--link">
-            Schedule <i class="fas fa-calendar-week"></i>
+            <Link to="/streams/info" className="header-nav--link">
+              Info <i class="fas fa-info-circle"></i>
             </Link>
           </li>
         </ul>
