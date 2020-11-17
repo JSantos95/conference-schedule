9f6dfa67e75fe9fa0b0fb74c911adfec539a5e57
Jose <santosjosel1995@gmail.com>
2020-11-17 13:54:15 -0500

    react-router-dom restructor, Speaker and Event moved under Main Link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './styles.css';
 import Header from './components/Header';
-import Event from './components/Event';
-import Speaker from './components/Speaker';
+import Main from './components/streams/Main';
+import Info from './components/streams/Info';
+import Schedule from './components/streams/Schedule';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Event />
-      <Speaker />
-      <Footer />
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path='/' exact component={Main} />
+          <Route path='/streams/info' exact component={Info} />
+          <Route path='/streams/schedule' exact component={Schedule} />
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
