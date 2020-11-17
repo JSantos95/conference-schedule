7aec8a100615b9ea993ffc080189dda7050766df
Jose <santosjosel1995@gmail.com>
2020-11-17 13:55:45 -0500

    React-Router streams

---
+++ src/components/streams/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Event from '../Event';
+import Speaker from '../Speaker';
+
+const Main = () => {
+  return (
+    <div>
+      <Event />
+      <Speaker />
+    </div>
+  )
+}
+
+export default Main;
