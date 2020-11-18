d0de8f3a95632c96c3759b47b4ee10e670f03d2a
Jose <santosjosel1995@gmail.com>
2020-11-18 01:05:31 -0500

    Added a Location and Food section to Info page 🔽🍔

--- src/components/streams/Info.js
+++ src/components/streams/Info.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="info">
+      <h2 className="info-title"><i class="fas fa-map-marker-alt"></i> Location</h2>
+      <p className="info-text">65-30 Kissena Blvd, Flushing, NY 11367</p>
+      <p className="info-text">Near the Queens College Diner</p>
+      <h2 className="info-title"><i class="fas fa-hamburger"></i> Food</h2>
+      <p className="info-text">We have Kosher <br/> We have Halah <br/> We have the Meats <br/> Abby's</p>
+    </div>
   )
 }
 
