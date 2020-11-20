2d06fe37a7cd54a3d75cb2344201fb8926be676b
Jose <santosjosel1995@gmail.com>
2020-11-20 15:16:57 -0500

    Rearranged some of the td and added classes 🔀

--- src/components/streams/Schedule.js
+++ src/components/streams/Schedule.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Schedule = () => {
   return (
-    <div>
-      <table>
+    <div class="schedule">
+      <h1 className="schedule-title">Our Schedule</h1>
+      <table className="schedule-table">
         <tr>
-          <th>X</th>
+          <th className="schedule-table--offset"></th>
           <th>Friday</th>
           <th>Saturday</th>
           <th>Sunday</th>
@@ -13,20 +14,20 @@ const Schedule = () => {
         <tr>
           <th>10:00-11:00</th>
           <td>HTML/CSS/JS 101</td>
-          <td>AWS Cloud</td>
           <td>React 101</td>
+          <td>AWS DevOps</td>
         </tr>
         <tr>
           <th>11:00-12:30</th>
           <td>CSS Flexbox/Grid 101</td>
-          <td>AWS DevOps</td>
+          <td>AWS Cloud</td>
           <td>React Hooks 101</td>
         </tr>
         <tr>
           <th>1:00-2:30</th>
           <td>Webpack NodeJS</td>
-          <td>AWS Developer</td>
           <td>Angular 101</td>
+          <td>AWS Developer</td>
         </tr>
         <tr>
           <th>3:00-4:30</th>
@@ -35,8 +36,14 @@ const Schedule = () => {
           <td>VueJs 101</td>
         </tr>
         <tr>
+          <th>5:00-6:00</th>
+          <td>NoSQL</td>
+          <td>Tailwind CSS</td>
+          <td>Wordpress</td>
+        </tr>
+        <tr>
           <th>6:00-7:30</th>
-          <td>Fun With PHP</td>
+          <td>Back End w/ PHP</td>
           <td>Amazon Alexa</td>
           <td>NextJS</td>
         </tr>
