dcdf4c643f4f87c9444e19be48d1703d8c663260
Jose <santosjosel1995@gmail.com>
2020-11-23 15:42:23 -0500

    Added Event schedule

--- src/components/streams/Schedule.js
+++ src/components/streams/Schedule.js
@@ -3,7 +3,24 @@ import React from 'react';
 const Schedule = () => {
   return (
     <div class="schedule">
-      <h1 className="schedule-title"><i class="fas fa-calendar-week"></i> Schedule</h1>
+      <h1 className="schedule-title"><i class="fas fa-calendar-week"></i> Event Schedule</h1>
+
+      <div className="schedule-days">
+        <div className="schedule-days--box">
+          <h3 className="schedule-days--title">Friday, May 6, 1985</h3>
+          <p className="schedule-days--text">9:00AM | Register Begins</p>
+          <p className="schedule-days--text">10:00AM | Workshops Begin</p>
+        </div>
+        <div className="schedule-days--box">
+          <h3 className="schedule-days--title">Saturday, May 7, 1985</h3>
+          <p className="schedule-days--text">10:00AM | Project Announcement</p>
+          <h3 className="schedule-days--title">Sunday, May 8, 1985</h3>
+          <p className="schedule-days--text">2:00PM | Projects Due</p>
+          <p className="schedule-days--text">5:00PM | Projects Awards</p>
+        </div>
+      </div>
+
+      <h1 className="schedule-title"><i class="fas fa-code"></i> Workshop Schedule</h1>
       <table className="schedule-table">
         <tr>
           <th className="schedule-table--offset"></th>
