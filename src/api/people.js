f2b77c03677ccb44e30804301199e44ff6b5fc31
Jose <santosjosel1995@gmail.com>
2020-11-24 22:48:09 -0500

    Needed '&' for multi parameters and limited results to English names

--- src/api/people.js
+++ src/api/people.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchPeople = () => {
   return axios
-    .get('https://randomuser.me/api/?inc=cell,name,picture?results=15')
+    .get('https://randomuser.me/api/?inc=cell,name,picture&results=15&nat=us')
     .then(({ data }) => {
       return data;
     })
