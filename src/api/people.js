c3ebccee94cc063f90e6db0ac16c5697fd5719c8
Jose <santosjosel1995@gmail.com>
2020-11-24 22:42:33 -0500

    limiting the data coming in to the pieces we need: name, cell, picture

--- src/api/people.js
+++ src/api/people.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchPeople = () => {
   return axios
-    .get('https://randomuser.me/api/?results=15')
+    .get('https://randomuser.me/api/?inc=cell,name,picture?results=15')
     .then(({ data }) => {
       return data;
     })
