5c0165866321af047522cf9a60d5b418e0809d99
Jose <santosjosel1995@gmail.com>
2020-12-02 13:49:22 -0500

    Added ScrollToTop hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './css/style.css';
+import ScrollToTop from './ScrollToTop';
 import Header from './components/Header';
 import Main from './components/streams/Main';
 import Info from './components/streams/Info';
 import Schedule from './components/streams/Schedule';
 import Footer from './components/Footer';
 
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
+          <ScrollToTop/>
           <Header />
           <Route path='/' exact component={Main} />
           <Route path='/streams/info' exact component={Info} />
