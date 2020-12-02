4e407a0f97263fe1b14c4da2fd2854ff7c0cd56b
Jose <santosjosel1995@gmail.com>
2020-12-02 01:08:30 -0500

    span to remove text in smaller mobile devices, no more popup nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,25 @@ import MiniNav from './MiniNav';
 const Header = () => {
   return (
     <div className="header-con">
-      <MiniNav/>
       <div className="header">
         <Link to="/" className="header-title-link">
           <h1 className="header-title">Techathon</h1>
         </Link>
 
-        <div className="header-miniNav">
-          <a href="#miniNav"><i className="fas fa-bars"></i></a>
-        </div>
-
         <ul className="header-nav">
           <li className="header-nav--item">
             <Link to="/" className="header-nav--link">
-              <i className="fas fa-home"></i> Home
+              <i className="fas fa-home"></i> <span className="header-nav--link_text">Home</span>
             </Link>
           </li>
           <li className="header-nav--item">
             <Link to="/streams/schedule" className="header-nav--link">
-              <i className="fas fa-calendar-week"></i> Schedule
+              <i className="fas fa-calendar-week"></i> <span className="header-nav--link_text">Schedule</span>
             </Link>
           </li>
           <li className="header-nav--item">
             <Link to="/streams/info" className="header-nav--link">
-             <i className="fas fa-info-circle"></i> Info
+             <i className="fas fa-info-circle"></i> <span className="header-nav--link_text">Info</span>
             </Link>
           </li>
         </ul>
